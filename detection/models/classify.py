3323f18f69de222c5bd0c8ddf26993c45166651d
dlcjfgmlnasa <dlcjfgmlnasa@naver.com>
2020-01-17 15:23:29 +0900

    성별, 나이 Classifiy 생성

--- detection/models/classify.py
+++ detection/models/classify.py
@@ -3,12 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as f
 
 
-class Output(nn.Module):
-    def __init__(self, input_size=2048):
+class SexClassify(nn.Module):
+    def __init__(self, input_size):
         super().__init__()
-        self.input_size = input_size
-        self.sex_branch = nn.Sequential(
-            nn.Linear(self.input_size, 4096),
+        self.classify = nn.Sequential(
+            nn.Linear(input_size, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),
             nn.Linear(4096, 4096),
@@ -16,8 +15,17 @@ class Output(nn.Module):
             nn.Dropout(p=0.5),
             nn.Linear(4096, 2)
         )
-        self.age_branch = nn.Sequential(
-            nn.Linear(self.input_size, 4096),
+
+    def forward(self, inputs):
+        x = self.classify(inputs)
+        return x
+
+
+class AgeClassify(nn.Module):
+    def __init__(self, input_size):
+        super().__init__()
+        self.classify = nn.Sequential(
+            nn.Linear(input_size, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),
             nn.Linear(4096, 4096),
@@ -27,8 +35,19 @@ class Output(nn.Module):
         )
 
     def forward(self, inputs):
-        sex_out = self.sex_branch(inputs)
-        age_out = self.age_branch(inputs)
-        sex_out = f.log_softmax(sex_out, dim=-1)
-        age_out = f.log_softmax(age_out, dim=-1)
+        x = self.classify(inputs)
+        return x
+
+
+class Classify(nn.Module):
+    def __init__(self, input_size=2048):
+        super().__init__()
+        self.sex_classify = SexClassify(input_size)
+        self.age_classify = AgeClassify(input_size)
+
+    def forward(self, inputs):
+        sex_out = self.sex_classify(inputs)
+        age_out = self.age_classify(inputs)
+        sex_out = f.softmax(sex_out, dim=-1)
+        age_out = f.softmax(age_out, dim=-1)
         return {'sex': sex_out, 'age': age_out}
