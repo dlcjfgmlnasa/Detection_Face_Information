1c5c5891da335d9e147b6da2414765db88c1552e
dlcjfgmlnasa <dlcjfgmlnasa@naver.com>
2020-02-07 13:09:16 +0900

    Update gitignore

--- raspberrypi/face_recognition.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-import cv2
-
-
-if __name__ == '__main__':
-    face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-    for line in open('./AFAD-Full/AFAD-Full.txt', encoding='utf-8'):
-        print(line)
